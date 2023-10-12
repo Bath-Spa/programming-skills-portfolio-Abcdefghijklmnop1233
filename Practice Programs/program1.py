180bd2322f58d84975ceda08418ec322dd110970
Abcdefghijklmnop1233 <145037446+Abcdefghijklmnop1233@users.noreply.github.com>
2023-10-12 08:20:04 -0700

    Create program1.py
    print

---
+++ Practice Programs/program1.py
@@ -0,0 +1,19 @@
+print("this is text")
+print(10/3)
+
+
+age = 21
+print("You are", age, "Years Old")
+
+
+a="Hello"
+b="Big"
+c="world"
+print(a, " " + b , " " + c)
+
+a= "hello"
+print(a*3)
+
+
+
+print('enter the amount of ' + 'sales for each day and' + 'press enter.') 
