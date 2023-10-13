08503ff576d19c0f342ba0c5e019dfea4c272755
Abcdefghijklmnop1233 <145037446+Abcdefghijklmnop1233@users.noreply.github.com>
2023-10-13 03:53:22 -0700

    Update program1.py

--- Practice Programs/program1.py
+++ Practice Programs/program1.py
@@ -1,7 +1,9 @@
+#This program is for Print function
 print("this is text")
 print(10/3)
 
 
+#this program is for creating variBLE and PRINTING It
 age = 21
 print("You are", age, "Years Old")
 
@@ -17,3 +19,18 @@ print(a*3)
 
 
 print('enter the amount of ' + 'sales for each day and' + 'press enter.') 
+
+
+my_str = 'one' 'two' 'three'
+print(my_str)
+
+#this program adds two numbers
+
+num1 = 1.5 
+num2 = 6.3
+
+#add tow numbers
+sum = num1 + num2 
+
+#display the sum
+print('the sum of[o] and [1] is [2]'.format(num1,num2,sum))
